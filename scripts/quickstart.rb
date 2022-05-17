6f16a01ffb74f38eecfc66a45d1b598d3222372b
Jackie Rosenzveig <jackie@cloudinary.com>
2022-05-17 12:23:01 +0300

    First draft

---
+++ scripts/quickstart.rb
@@ -0,0 +1,108 @@
+require 'cloudinary'
+
+# Cloudinary.config_from_url("cloudinary://API_KEY:API_SECRET@CLOUD_NAME")
+Cloudinary.config_from_url("cloudinary://381919499616147:wYlStJKsZj7e5_knU3MVFI2jk-M@jackie")
+Cloudinary.config do |config|
+  config.secure = true
+end
+
+puts 'My cloud name is:' + Cloudinary.config.cloud_name
+
+## Upload 2 files using the Upload API:
+
+# use the uploaded filename as the asset's public ID + allow overwriting asset with new versions.
+upload1=Cloudinary::Uploader.upload("https://res.cloudinary.com/demo/image/upload/docs/model.jpg", 
+    use_filename:true, 
+    unique_filename:false,
+    overwrite:true
+    )
+
+# retrieve some attributes from the first upload response
+
+    puts "File size of the #{upload1['public_id']} image: #{upload1['bytes']/1024} KB"      
+
+# manually set the asset's public ID + allow overwriting asset with new versions.
+upload2=Cloudinary::Uploader.upload("https://res.cloudinary.com/demo/image/upload/coffee_cup.jpg", 
+    public_id: "coffee", 
+    unique_filename: false,
+    overwrite: true
+    )
+
+
+# retrieve some attributes from the second upload response
+    puts "File size of the #{upload2['public_id']} image: #{upload2['bytes']/1024} KB"          
+   
+
+## Manage existing assets using the Admin API
+
+# retrieve attributes about an uploaded asset, based on the returned public ID.
+my_asset = upload1['public_id']
+
+puts %{
+Asset details:
+=================
+
+}
+
+
+jj details = Cloudinary::Api.resource(my_asset,
+:faces => true)
+
+puts %{
+================================
+
+}
+
+# find out how many faces are in the image, and depending on result, tag the image accordingly.
+facecount = details['faces'].count
+
+puts "Number of faces in this image: #{facecount}"  
+
+if facecount > 0
+  asset_update=Cloudinary::Api.update(my_asset,
+  :tags => "people")
+else 
+  asset_update=Cloudinary::Api.update(my_asset,
+  :tags => "products")
+end
+
+asset_tags = asset_update['tags']
+
+puts "Asset tags: #{asset_tags}"  
+
+puts %{
+
+Generated transformation URLs: 
+================================
+
+}
+
+## Generate transformation URLs for previously uploaded assets
+
+my_asset1 = upload1['public_id']+'.jpg'
+my_asset2 = upload2['public_id']+'.jpg'
+
+
+# scale and then crop the excess to 'fill' 640*200px banner-shape, focussed on the largest face (if any). 
+puts Cloudinary::Utils.cloudinary_url(my_asset1, :transformation=>
+    {:crop=>"fill", :gravity=>"face", :width=>640, :height=>200,  }
+  )
+
+# create a 200px square (1:1 aspect ratio) thumbnail, then round to a circle to get a profile photo of the face
+puts Cloudinary::Utils.cloudinary_url(my_asset1, :transformation=>[
+    {:crop=>"thumb", :gravity=>"face",:width=>200, :aspect_ratio=>"1.0"},
+    {:radius=>"max"}
+])
+
+# deliver a URL link to the original of the second image.
+puts Cloudinary::Utils.cloudinary_url(my_asset2)
+
+# scale & crop to a portrait image (6:10 aspect ratio), apply a sepia effect, 
+# and automatically optimize the image quality and file format to best balance 
+# between visual quality and small file size
+puts Cloudinary::Utils.cloudinary_url(my_asset2, :transformation=>[
+    {:aspect_ratio=>"0.6", :width=>400, :crop=>"fill"},
+    {:effect=>"sepia"},
+    {:fetch_format=>:auto},
+    {:quality=>:auto}
+    ])
