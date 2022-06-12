17c98a6860d76a5e4fd7b5640e034f4daf8a08e9
Jackie Rosenzveig <jackie@cloudinary.com>
2022-06-12 14:13:22 +0300

    Update comment

--- config/config.rb
+++ config/config.rb
@@ -1,6 +1,9 @@
+# Replace the 'config_from_url' string value below with your 
+# account's credentials, available from your Cloudinary console.
+# =====================================================
+
 require 'cloudinary'
 
-# Update the config string below with your credentials, available from your Cloudinary console.
 Cloudinary.config_from_url("cloudinary://API_KEY:API_SECRET@CLOUD_NAME")
 Cloudinary.config do |config|
   config.secure = true
