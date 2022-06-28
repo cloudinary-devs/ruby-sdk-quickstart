7bc0d8d9b47cc18914ee86e51b0b4e07a32f1b34
Jackie Rosenzveig <jackie@cloudinary.com>
2022-06-28 12:26:53 +0300

    Update upload console texts to use more response attributes

--- quickstart.rb
+++ quickstart.rb
@@ -17,18 +17,18 @@ upload1=Cloudinary::Uploader.upload("https://cloudinary-devs.github.io/cld-docs-
 
 # Retrieve some attributes from the first upload response.
 
-    puts "File size of the #{upload1['public_id']} image: #{upload1['bytes']/1024} KB"     
+  puts "File size of the #{upload1['public_id']}.#{upload1['format']} #{upload1['resource_type']}: #{upload1['bytes']/1024} KB"     
 
 # Manually set the asset's public ID + allow overwriting asset with new versions.
-upload2=Cloudinary::Uploader.upload("https://cloudinary-devs.github.io/cld-docs-assets/assets/images/coffee_cup.jpg", 
-    public_id: "coffee", 
-    unique_filename: false,
-    overwrite: true
-    )
+  upload2=Cloudinary::Uploader.upload("https://cloudinary-devs.github.io/cld-docs-assets/assets/images/coffee_cup.jpg", 
+      public_id: "coffee", 
+      unique_filename: false,
+      overwrite: true
+      )
 
 
 # Retrieve some attributes from the second upload response.
-    puts "File size of the #{upload2['public_id']} image: #{upload2['bytes']/1024} KB"          
+  puts "File size of the #{upload2['public_id']}.#{upload2['format']} #{upload2['resource_type']}: #{upload2['bytes']/1024} KB"          
    
 
 ## Manage existing assets using the Admin API
